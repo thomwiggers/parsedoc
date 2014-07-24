ac6ebe3a62ea6d5110a91752abf635e4c1f9a664
Thom Wiggers <thom@thomwiggers.nl>
2014-07-24 19:04:08 +0200

    improvements, more tests

--- parsedoc/parser.py
+++ parsedoc/parser.py
@@ -7,7 +7,7 @@ from __future__ import unicode_literals, print_function, absolute_import
 import re
 import logging
 
-from .blocks import *
+from .blocks import PHPFile, PHPClass, PHPFunction
 
 
 def parse_file(file_name, file_contents):
@@ -28,59 +28,72 @@ def get_file_comment(file_contents):
     regexp = re.compile(r"""
         ^\s*<\?(php)?\s*\n          # File start
         (?P<docblock>/\*\*.*?\*/)?  # documentation on line 2
+        [ ]*\n                      # newline
         (?P<rest>.*)$               # Rest of the file
         """, re.VERBOSE | re.DOTALL)
     result = regexp.match(file_contents)
     if result:
         file_comment = result.group('docblock')
         logging.debug("Found comment: {}".format(file_comment))
-
-    return file_comment, result.group('rest')
+        return file_comment, result.group('rest').strip()
+    else:
+        return None, file_contents
 
 
 def get_function_or_class(file_contents):
     """Get one function or class from file_contents"""
-    comment = ""
     regexp = re.compile(r"""
         .*?                            # Eat up everything that isn't a
                                        # function or class
         ((?P<docblock>/\*\*.*?\*/)     # find docblock
-        [ ]*\n)?                        # eat space and newline
-        ((?P<function>function .*?{) | # find a function
+        [ ]*\n)?                       # eat space and newline
+        [ ]*                           # eat any prefix spaces
+        ((?P<function>                 # find a function
+            (static|public|private|protected|[ ])*
+            function .*?{          ) |
          (?P<class>class .*? {))       # or a class
         (?P<rest>.*)                   # rest of the file
-        """, re.VERBOSE | re.DOTALL | re.MULTILINE)
+        """, re.VERBOSE | re.DOTALL)
     result = regexp.match(file_contents)
 
     if not result:
         return None, ""
 
+    if result.group('class'):
+        return parse_class(result, file_contents)
+    elif result.group('function'):
+        comment = ""
+        if result.group('docblock'):
+            comment = result.group('docblock')
+        return (PHPFunction(result.group('function'), comment),
+                result.group('rest').strip())
+
+
+def parse_class(result, file_contents):
+    comment = ""
     if result.group('docblock'):
         comment = result.group('docblock')
+    """Parse a class"""
+    # find contents of class:
+    stack = 1
+    accum = ''
+    for c in result.group('rest'):
+        accum += c
+        if c == '}':
+            stack -= 1
+        elif c == '{':
+            stack += 1
 
-    if result.group('class'):
-        # find contents of class:
-        stack = 1
-        accum = ''
-        for c in result.group('rest'):
-            accum += c
-            if c == '}':
-                stack -= 1
-            elif c == '{':
-                stack += 1
-
-            if stack == 0:
-                break
-        if len(accum) < len(result.group('rest')):
-            rest = result.group('rest')[len(accum)]
-        else:
-            rest = ''
-        class_contains = []
-        while accum:
-            block, accum = get_functions_and_classes(accum)
-            if block:
-                class_contains.append(block)
-        return PHPClass(result.group('class'), comment, class_contains), rest
-    elif result.group('function'):
-        return (PHPFunction(result.group('function'), comment),
-                result.group('rest'))
+        if stack == 0:
+            break
+    if len(accum) < len(result.group('rest')):
+        rest = result.group('rest')[len(accum):]
+    else:
+        rest = ''
+    class_contains = []
+    while accum:
+        block, accum = get_function_or_class(accum)
+        if block:
+            class_contains.append(block)
+    return (PHPClass(result.group('class'), comment, class_contains),
+            rest.strip())
--- tests/test_parser.py
+++ tests/test_parser.py
@@ -1,7 +1,5 @@
 # -*- coding: utf-8 -*-
-from unittest import TestCase
-
-from parsedoc.parser import *
+from parsedoc.parser import parse_file, get_function_or_class, get_file_comment
 from parsedoc import blocks
 
 
@@ -29,6 +27,7 @@ function foo_function($arg1, $arg2) {
     assert "Function 1 comment" in function.comment
     assert len(function.contains) == 0
 
+
 def test_file_uncommented():
     result = parse_file("bar.php", """<?
 function hi() {
@@ -38,6 +37,7 @@ function hi() {
     assert result.name == "bar.php"
     assert result.comment is None
     assert len(result.contains) == 1
+
 
 def test_get_file_comment():
     result, rest = get_file_comment("""<?php
@@ -49,10 +49,12 @@ blabla""")
     assert "File comment" in result
     assert "blabla" in rest
 
+
 def test_get_file_comment_no_comment():
     result, rest = get_file_comment("<?php\nfunction foo(){};")
     assert result is None
-    assert "function foo(){};" == rest
+    assert "function foo(){};" in rest
+
 
 def test_get_file_comment_no_php():
     result, rest = get_file_comment("<? """"
@@ -64,10 +66,197 @@ blabla""")
     assert "File comment" in result
     assert "blabla" in rest
 
+
 def test_get_file_comment_empty_rest():
     result, rest = get_file_comment("""<?php
 /**
  * File comment
- */""")
+ */
+    """)
     assert "File comment" in result
     assert rest == ""
+
+
+def test_file_with_class():
+    result, rest = parse_file(
+        "foo.php",
+        """<?php
+        /**
+         * File comment
+         */
+
+        /**
+         * Test Class
+         */
+        class TestClass
+        {
+            /**
+             * test function
+             */
+            function foo_function() {
+                //content
+            }
+        }
+        """)
+    assert type(result) == blocks.PHPFile
+    assert "File comment" in result.comment
+    assert len(result.contains) == 1
+    assert type(result.contains[0]) == blocks.PHPClass
+    block = result.contains[0]
+    assert "Test Class" in block.comment
+    assert "TestClass" in block.name
+    assert len(block.contains) == 1
+    block = block.contains[0]
+    assert "test function" in block.comment
+    assert "foo_function" in block.name
+    assert len(block.contains) == 0
+
+
+def test_function_comment():
+    result, rest = get_function_or_class(
+        """
+        /**
+         * Function 1 comment
+         */
+        function foo_function() {
+            // function body
+        }
+        """)
+    assert type(result) == blocks.PHPFunction
+    assert "foo_function" in result.name
+    assert "Function 1 comment" in result.comment
+    assert len(result.contains) == 0
+
+
+def test_class_comment():
+    result, _ = get_function_or_class(
+        """
+        /**
+         * Class comment
+         */
+        class FooClass {
+
+            /**
+             * foo function
+             */
+            function foo_function () {
+                // contents
+            }
+            // other class contents
+        }
+        """)
+    assert type(result) == blocks.PHPClass
+    assert "FooClass" in result.name
+    assert "Class comment" in result.comment
+    assert len(result.contains) == 1
+    function = result.contains[0]
+    assert type(function) == blocks.PHPFunction
+    assert "foo_function" in function.name
+    assert "foo function" in function.comment
+    assert len(function.contains) == 0
+
+
+def test_class_comment_two_functions():
+    result, _ = get_function_or_class(
+        """
+        /**
+         * Class comment
+         */
+        class FooClass {
+
+            /**
+             * foo function 1
+             */
+            function foo_function_1 () {
+                // contents
+            }
+            /**
+             * foo function 2
+             */
+            function foo_function_2 () {
+                // contents
+            }
+           // other class contents
+        }
+        """)
+    assert type(result) == blocks.PHPClass
+    assert "FooClass" in result.name
+    assert "Class comment" in result.comment
+    assert len(result.contains) == 2
+    function = result.contains[0]
+    assert type(function) == blocks.PHPFunction
+    assert "foo_function_1" in function.name
+    assert "foo function 1" in function.comment
+    assert len(function.contains) == 0
+    function = result.contains[1]
+    assert type(function) == blocks.PHPFunction
+    assert "foo_function_2" in function.name
+    assert "foo function 2" in function.comment
+    assert len(function.contains) == 0
+
+
+def test_class_comment_two_functions_one_uncommented():
+    result, _ = get_function_or_class(
+        """
+        /**
+         * Class comment
+         */
+        class FooClass {
+
+            /**
+             * foo function 1
+             */
+            function foo_function_1 () {
+                // contents
+            }
+            function foo_function_2 () {
+                // contents
+            }
+           // other class contents
+        }
+        """)
+    assert type(result) == blocks.PHPClass
+    assert "FooClass" in result.name
+    assert "Class comment" in result.comment
+    assert len(result.contains) == 2
+    function = result.contains[0]
+    assert type(function) == blocks.PHPFunction
+    assert "foo_function_1" in function.name
+    assert "foo function 1" in function.comment
+    assert len(function.contains) == 0
+    function = result.contains[1]
+    assert type(function) == blocks.PHPFunction
+    assert "foo_function_2" in function.name
+    assert function.comment == ""
+
+
+def test_class_comment_two_functions_class_uncommented():
+    result, _ = get_function_or_class(
+        """
+        class FooClass {
+
+            /**
+             * foo function 1
+             */
+            function foo_function_1 () {
+                // contents
+            }
+            function foo_function_2 () {
+                // contents
+            }
+           // other class contents
+        }
+        """)
+    assert type(result) == blocks.PHPClass
+    assert "FooClass" in result.name
+    assert result.comment == ""
+    assert len(result.contains) == 2
+    function = result.contains[0]
+    assert type(function) == blocks.PHPFunction
+    assert "foo_function_1" in function.name
+    assert "foo function 1" in function.comment
+    assert len(function.contains) == 0
+    function = result.contains[1]
+    assert type(function) == blocks.PHPFunction
+    assert "foo_function_2" in function.name
+    assert function.comment == ""
